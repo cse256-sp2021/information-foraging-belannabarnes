4581a6fbd1f2813389a8e26d886662702ba9a3c1
belannabarnes <46663548+belannabarnes@users.noreply.github.com>
2021-02-28 12:06:27 -0600

    sticky header added

--- scripts/ts/dynamic-dom/dynamic-dom.ts
+++ scripts/ts/dynamic-dom/dynamic-dom.ts
@@ -3,6 +3,7 @@ import { Accordion } from './accordion';
 import { doSomething } from './do-something';
 import { HTMLContent, itemsToCache } from './html-imports';
 import { Slideshow } from './slideshow';
+import { setupSticky } from './sticky';
 
 // Put all function calls that need to be made on every page load inside the setupAll function body.
 export function PutStudentPageLoadOperationsInsideThisStudentBody() {
@@ -18,6 +19,7 @@ export async function setupAll() {
     Accordion.setupAll();
     PutStudentPageLoadOperationsInsideThisStudentBody();
     console.log('reloaded');
+    setupSticky();
 }
 
 itemsToCache.forEach((item: HTMLContent) => {
